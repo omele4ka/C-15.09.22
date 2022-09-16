85eb16299217771cfee3eae97405e5d0f16b6e92
Omele4ka <omele4ka@inbox.ru>
2022-09-16 13:36:41 +0300

    Add Task 07 & solution

---
+++ Task07/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трехщначное число,
+// а на выходе показывает последнюю цифру этого числа
+
+Console.WriteLine("Введите трехначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num % 10;
+Console.WriteLine($"Последняя цифра {result}");
+
