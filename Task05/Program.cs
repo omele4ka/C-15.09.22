90169117999f9b60e9b0432092160168118bf439
Omele4ka <omele4ka@inbox.ru>
2022-09-16 13:20:59 +0300

    Add Task 05 & solution

---
+++ Task05/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int counter = -num;
+while (counter <= num)
+{
+Console.Write($"{counter} ");
+counter++;
+}
