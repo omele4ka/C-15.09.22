63c15987cbfd5246a07d2fae15df46c4aaad0334
Omele4ka <omele4ka@inbox.ru>
2022-09-16 13:30:40 +0300

    Task 05 add conditions & solution

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Введите целое число: ");
+﻿// Написать программу, которая на вход принимает одно число (N), 
+// а на выходе выводит все целые числа в промежутке от -N до N
+
+
+Console.WriteLine("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int counter = -num;
 while (counter <= num)
