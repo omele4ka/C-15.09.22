6fef28738e8de6da9daf6a1cca25fadcc1e3ed92
Omele4ka <omele4ka@inbox.ru>
2022-09-22 12:54:38 +0300

    Add Task 18 & solution

---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите четверть: ");
+int quater = Convert.ToInt32(Console.ReadLine());
+string result = Diaposon(quater);
+
+string Diaposon(int quater1)
+{
+    if (quater1 == 1) return "x > 0, y > 0";
+    if (quater1 == 2) return "x < 0, y > 0";
+    if (quater1 == 3) return "x < 0, y < 0";
+    if (quater1 == 4) return "x > 0, y < 0";
+    return "Введены некорректные данные";
+}
+
+Console.WriteLine(result);
