3fa4850fb034eb0432943604a6bc474bfd5c1fc5
Omele4ka <omele4ka@inbox.ru>
2022-09-28 23:07:53 +0300

    Add Task 28 & solution

---
+++ Task28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int FindMultiply(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+if (num > 0)
+{
+    int multiply = FindMultiply(num);
+    Console.WriteLine(multiply);
+}
+
+else Console.WriteLine("Введено некорректное значение");
