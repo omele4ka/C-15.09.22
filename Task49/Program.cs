9127c95a9fe9260f65e3776ec5be5875de9fdc2f
Omele4ka <omele4ka@inbox.ru>
2022-10-06 12:26:02 +0300

    Add Task 49 & solution

---
+++ Task49/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4}"); // 4 - форматирование вывода элементов в консоль
+        }
+    
+        Console.WriteLine("]");
+    }
+}
+
+void GetSquareEvenElem (int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j+=2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+int[,] getSquareEvenElem = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(getSquareEvenElem);
+Console.WriteLine();
+
+GetSquareEvenElem(getSquareEvenElem);
+PrintMatrix(getSquareEvenElem);
